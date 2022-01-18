551606a963783dac9d9d702d0ae15901ef09e864
osterhase0815 <hans.volpe@gmx.de>
2022-01-18 09:06:26 +0100

    Master Slave  working

--- Exercises/TerraformAzureMariaDB/variables.tf
+++ Exercises/TerraformAzureMariaDB/variables.tf
@@ -6,33 +6,42 @@
 
 variable "location_spread" {
     default = [ "westeurope", "northeurope"]
-} 
+}
 
 variable "account_location" {
   default = "westeurope"
 }
- 
+
 variable "resource_group_name" {
   default = "maria-resources"
 }
- 
+
 variable "maria_host_pre" {
-  default = "aw0815-maria"
+  default = "mvb4712-maria"
 }
- 
+
 variable "maria_fqdn_post" {
   default = ".westeurope.cloudapp.azure.com"
 }
- 
+
 variable "maria_srv_count" {
   default = 2
 }
- 
+
 
 variable "maria_usr_name" {
   default = "sqladmin"
 }
- 
+
 variable "maria_usr_pwd" {
   default = "blackmaria"
 }
+
+variable "maria_rep_name" {
+  default = "repl"
+}
+
+variable "maria_rep_pwd" {
+  default = "Hallabalu1806!"
+}
+
